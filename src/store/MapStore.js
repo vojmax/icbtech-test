6015bd4fc4fb48756ba514297bbcf6d82bab0411
vojmax <vojislav.maksovic@gmail.com>
2024-03-12 13:56:48 +0100

    now on city change we have array that keeps all bilboard and we filter that array to show bilboards from different cities in modal

--- src/store/MapStore.js
+++ src/store/MapStore.js
@@ -4,18 +4,20 @@ import { useCitiesStore } from './CitiesStore'
 
 export const useMapStore = defineStore('MapStore', () => {
   const { selectedCity } = storeToRefs(useCitiesStore()) // useCitiesStore() returns the store instance
+  const allBilboards = ref([]) // array of all bilboards from all cities that updated when needed
   const bilboards = ref([]) // reactive bilboards array
   const selectMore = ref(false) // reactive selectMore flag
-  const oneBilboard = ref(null)
+  const oneBilboard = ref(null) //bilboard object for openModal function found by id
   const selectedBilboards = ref([]) // array of checked bilboards ids
   const filterBilboardsById = ref([]) // reactive filterBilboardsById object
   const bilboardCenter = ref({ lat: 46.10078231214396, lng: 19.66944742489657 }) // reactive bilboardCenter object with default value
 
   watch(selectedBilboards, () => {
-    filterBilboardsById.value = bilboards.value.filter((bilboard) =>
+    filterBilboardsById.value = allBilboards.value.filter((bilboard) =>
       selectedBilboards.value.includes(bilboard.id)
     ) // filter bilboards by selectedBilboards whenever selectedBilboards changes
     console.log('filterBilboardsById changed:', filterBilboardsById.value)
+    console.log('selectedBilboards changed:', allBilboards.value)
   })
 
   watch(bilboards, () => {
@@ -40,9 +42,31 @@ export const useMapStore = defineStore('MapStore', () => {
   })
 
   watch(selectedCity, () => {
-    selectedBilboards.value = [] // reset selectedBilboards whenever selectedCity changes
     fetchBilboards() // fetch bilboards when selectedCity changes
+    updateAllBilboards() //update allBilboards array
   })
+
+  //update allBilboards array when on mount and when city changes
+  //keep the value only if it's not already in the array
+  //then we can filter this array and show bilboards from diffent cities in modal
+  const updateAllBilboards = async () => {
+    const url = 'https://ledbilbordi.web.icbtech.net/api/v1/billboard-list/' + selectedCity.value
+
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log('all bilboards', allBilboards.value)
+      allBilboards.value = [
+        ...allBilboards.value,
+        ...data.filter(
+          (bilboard) => !allBilboards.value.some((existing) => existing.id === bilboard.id)
+        )
+      ]
+      console.log('all bilboards', allBilboards.value)
+    } catch (error) {
+      console.error('Failed to fetch bilboards data:', error)
+    }
+  }
 
   const fetchBilboards = async () => {
     const url = 'https://ledbilbordi.web.icbtech.net/api/v1/billboard-list/' + selectedCity.value // fetch bilboards data from the server
@@ -58,6 +82,7 @@ export const useMapStore = defineStore('MapStore', () => {
 
   onMounted(() => {
     fetchBilboards() // fetch bilboards when the component is mounted
+    updateAllBilboards()
   })
   return {
     bilboards,
